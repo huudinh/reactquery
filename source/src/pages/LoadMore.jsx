df1ddbe8fbeccaa2d59a845126349c784c150871
huudinh <dinhndvn@yahoo.com>
2024-07-08 11:44:53 +0700

    session_007_loadmore

--- source/src/pages/LoadMore.jsx
+++ source/src/pages/LoadMore.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useQuery, keepPreviousData, useInfiniteQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import Header from '../components/Header';
 import { useState } from 'react';
 
